9d67e82c98e319bf0e94577a683bd7719de45226
deadprogram <ron@hybridgroup.com>
2016-09-26 22:22:36 +0200

    Add LCD display to sensor demo
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- demos/sensores.js
+++ demos/sensores.js
@@ -8,7 +8,8 @@ Cylon.robot({
   devices: {
     termo: { driver: "temperature-sensor", pin: 0, connection: "placa" },
     luz: { driver: "analog-sensor", pin: 1, connection: "placa" },
-    dial: { driver: "analog-sensor", pin: 2, connection: "placa" }
+    dial: { driver: "analog-sensor", pin: 2, connection: "placa" },
+    pantalla: { driver: "jhd1313m1", connection: "placa" }
   },
 
   work: function(mi) {
@@ -24,7 +25,13 @@ Cylon.robot({
     });
 
     mi.dial.on("analogRead", function(val) {
-      console.log("dial valor:", val);
+      var str = "dial: "+ val.toString();
+      while (str.length < 16) {
+        str = str + " ";
+      }
+      console.log(str);
+      mi.pantalla.setCursor(0,0);
+      mi.pantalla.write(str);
       mi.servidor.publish("sensores/dial", val.toString());
     });
   }
